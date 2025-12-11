50df64460b13c9c374c45837c9830c2cf98fba0a
Faheemmalla <faheem.yousuf2004@gmail.com>
2025-12-11 08:59:36 +0530

    Initial Commit

---
+++ BMI-Predictor/BMI_PredictorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_PredictorApp.swift
+//  BMI-Predictor
+//
+//  Created by faheem yousuf malla on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct BMI_PredictorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BMI-Predictor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BMI-Predictor
+//
+//  Created by faheem yousuf malla on 11/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
